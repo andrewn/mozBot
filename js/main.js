ff6d0fb36d1226609de7f503035dcf25547c4d4c
Jerome Etienne <jerome.etienne@gmail.com>
2011-11-05 13:09:28 +0100

    removed body part + highlight on hover

--- js/main.js
+++ js/main.js
@@ -74,7 +74,10 @@ $(document).ready(function(){
 		camParent		= new THREE.Object3D();
 		modelParent		= new THREE.Object3D();
 		modelParent.rotation.x = -Math.PI/2;
-		
+
+		// init projector to get mouse click on mesh
+		projector = new THREE.Projector();
+				
 		// Create the scene
 		scene		= new THREE.Scene();
 		scene.fog	= new THREE.Fog(blue, sceneHalf-300, sceneHalf);
@@ -84,13 +87,6 @@ $(document).ready(function(){
 		camParent.addChild(camera);
 		
 		scene.addObject(camParent);
-		
-		//facemat = new THREE.MeshBasicMaterial( { color: white, opacity: 1.0, shading: THREE.FlatShading } );
-		facemat = new THREE.MeshLambertMaterial( { color: white, opacity: 1.0 } );
-		//wiremat = new THREE.MeshBasicMaterial( { color: blue, opacity: 1.0, wireframe: true, wireframeLinewidth: 1.0 } );
-		
-		//Material = [facemat,wiremat]; 
-		Material = [facemat]; 
 		
 		// SET UP LIGHTS
 		d1		= new THREE.DirectionalLight( 0xE0E0E0);
@@ -115,7 +111,10 @@ $(document).ready(function(){
 		$('#container').show();
 		
 		canvas = document.getElementById('canvas');
-		
+
+		canvas.addEventListener( 'mousemove', onDocumentMouseMove, false );
+		canvas.addEventListener( 'mousedown', onDocumentMouseDown2, false );
+
 		window.addEventListener('resize', onWindowResize, false);
 		canvas.addEventListener( 'mousedown', onDocumentMouseDown, false );
 		canvas.addEventListener( 'touchstart', onDocumentTouchStart, false );
@@ -140,6 +139,14 @@ function makeRobot(segments){
 	part.useQuaternion = true;
 	var currentTime = new Date()
 	part.birthTime = currentTime.getTime();
+
+	// init collider
+	var mesh	= part;
+	var collider	= THREE.CollisionUtils.MeshColliderWBox(mesh);
+	THREE.Collisions.colliders.push( collider );
+	collidableMeshes.push(mesh);
+	mesh._bodyPart	= "torso";
+	mesh._collider	= collider;	
 	
 	modelParent.addChild(part);
 	
@@ -149,7 +156,16 @@ function makeRobot(segments){
 	
 	// Loop through all the limbs and create a part for each
 	for (var i = 0; i < partList.length; i ++ ){
-		makePart(partList[i], connectors);
+		part = makePart(partList[i], connectors);
+
+		// init collider
+		var mesh	= part;
+		var collider	= THREE.CollisionUtils.MeshColliderWBox(mesh);
+		THREE.Collisions.colliders.push( collider );
+		collidableMeshes.push(mesh);
+
+		mesh._bodyPart	= partList[i];
+		mesh._collider	= collider;	
 	}
 	
 }
@@ -175,11 +191,31 @@ function makePart(type, connectors){
 	
 	Models[type] = part;
 
+	return part;
 }
 
+function nextPart(mesh)
+{
+	console.log("nextPart", mesh._bodyPart);
+	//modelParent.removeChild(mesh);
+	scene.removeChild(mesh);
+
+	var colliders	= THREE.Collisions.colliders;
+	var position	= colliders.indexOf(mesh._collider);
+console.log("position", position);
+	colliders.splice( position, 1 );
+	
+}
 
 // Make the mesh!
 function makeMesh(bot,type){
+	//facemat = new THREE.MeshBasicMaterial( { color: white, opacity: 1.0, shading: THREE.FlatShading } );
+	var facemat = new THREE.MeshLambertMaterial( { color: 0xFFFFFF, opacity: 1.0 } );
+	//wiremat = new THREE.MeshBasicMaterial( { color: blue, opacity: 1.0, wireframe: true, wireframeLinewidth: 1.0 } );
+	
+	//Material = [facemat,wiremat]; 
+	var Material = [facemat]; 
+		
 	if(bot == 'round') return new THREE.Mesh( new round(type),  Material);
 	return new THREE.Mesh( new base(type),  Material);
 }
@@ -192,10 +228,47 @@ function animate() {
 	if(debug)	stats.update();
 }
 
+var mouse = { x: undefined, y: undefined };
+function onDocumentMouseMove( event ) {
+
+	event.preventDefault();
+	mouse.x =   ( event.clientX / window.innerWidth  ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+};
+
+function onDocumentMouseDown2( event ) {
+	console.log("selectedMesh", selectedMesh)
+	if( !selectedMesh )	return;
+	nextPart(selectedMesh);
+}
 
 // Each render of a frame!
 // We update the models in here
 function render() {
+	
+(function(){
+	// reset all the colors
+	for(var i = 0; i < collidableMeshes.length; i++ ) {
+		collidableMeshes[ i ].materials[ 0 ].color.setHex( 0xFFFFFF );
+	}
+
+
+	var vector	= new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+	projector.unprojectVector( vector, camera );
+	var ray		= new THREE.Ray( camera.position, vector.subSelf( camera.position ).normalize() );
+	var collision	= THREE.Collisions.rayCastNearest( ray );
+	if( collision && mouse.x !== undefined ){
+		var mesh	= collision.mesh;
+		//info.innerHTML += "Found @ distance " + c.distance;
+		mesh.materials[ 0 ].color.setHex( 0xbb0000 );
+		//console.log("bbb", mouseState, mesh._bodyPart);
+		selectedMesh	= mesh;
+	} else {
+		selectedMesh	= null;
+	}
+	//console.log("selectedMesh", selectedMesh);
+})();
 
 	var now		= Date.now();
 	timeSpent	= now - oldNow;
